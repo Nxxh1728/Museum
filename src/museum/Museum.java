0064d8fbb3e6368086e6af600270a68a551f37c2
Nxxh1728 <noahmavrantzas@gmail.com>
2025-03-26 12:04:25 -0400

    ticket room

--- src/museum/Museum.java
+++ src/museum/Museum.java
@@ -19,8 +19,8 @@ public class Museum extends JPanel {
     private Canvas3D canvas;
     private TransformGroup viewTransform;
     private Transform3D viewTM = new Transform3D();
-    private Point3d camera = new Point3d(3.75, .35, -1); // Start at X = 4.5
-    private Point3d centerPoint = new Point3d(5, .35, -1); // Look forward along the X-axis
+    private Point3d camera = new Point3d(3.75, .35, -1); 
+    private Point3d centerPoint = new Point3d(5, .35, -1); 
     private Vector3d upDir = new Vector3d(0, 1, 0);
     private Movement movement;
     public final static Color3f White = new Color3f(1.0f, 1.0f, 1.0f);
--- src/museum/TicketRoom.java
+++ src/museum/TicketRoom.java
@@ -1,12 +1,26 @@
 package museum;
 
+import java.awt.Font;
+
+import org.jogamp.java3d.Appearance;
 import org.jogamp.java3d.BranchGroup;
+import org.jogamp.java3d.Font3D;
+import org.jogamp.java3d.FontExtrusion;
+import org.jogamp.java3d.Shape3D;
+import org.jogamp.java3d.Text3D;
 import org.jogamp.java3d.TransformGroup;
+import org.jogamp.java3d.utils.geometry.Box;
 import org.jogamp.java3d.Transform3D;
 import org.jogamp.vecmath.Vector3f;
 import org.jogamp.vecmath.AxisAngle4f;
+import org.jogamp.vecmath.Color3f;
 
 public class TicketRoom {
+	
+    private static final Appearance red = GameObjects.set_Appearance(new Color3f(1.0f, 0.0f, 0.0f));
+    private static final Appearance green = GameObjects.set_Appearance(new Color3f(0.0f, 1.0f, 0.0f));
+    
+    
     public static BranchGroup createTicketRoom() {
         BranchGroup ticketRoomGroup = new BranchGroup();
 
@@ -33,6 +47,22 @@ public class TicketRoom {
         boothTransformGroup.addChild(booth(new Vector3f(-1f, .5f, 4f))); 
         ticketRoomGroup.addChild(boothTransformGroup);
         
+        //click box
+        ticketRoomGroup.addChild(createBox(new Vector3f(4f, 0.25f, 1f), red));
+        
+                    
+        TransformGroup textTransformGroup = new TransformGroup();
+        textTransformGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        Transform3D textTransform = new Transform3D();
+        Transform3D rotation2 = new Transform3D();
+        rotation2.rotY(Math.PI ); // Rotate 45 degrees around Y-axis
+        textTransformGroup.getTransform(textTransform);
+        textTransform.mul(rotation2);
+        textTransformGroup.setTransform(textTransform); 
+
+        textTransformGroup.addChild(createText("Tickets", new Vector3f(-4f, .7f, -0.6f), 0.1f, new Color3f(1f, .9f, 0f)));
+        ticketRoomGroup.addChild(textTransformGroup);
+        
         return ticketRoomGroup;
         
         
@@ -58,6 +88,49 @@ public class TicketRoom {
         doorTransformGroup.setTransform(doorTransform);
         doorTransformGroup.addChild(door);
 
+        
+        
         return doorTransformGroup;
+    }
+    
+    public static TransformGroup createBox(Vector3f position, Appearance appearance) {
+        Transform3D transform = new Transform3D();
+        transform.setTranslation(position);
+
+        TransformGroup tg = new TransformGroup();
+        tg.setTransform(transform);
+
+        // Create a box with the specified appearance
+        Box box = new Box(0.1f, 0.25f, 0.1f, Box.GENERATE_NORMALS, appearance);
+
+        tg.addChild(box);
+        return tg;
+    }
+    
+    public static TransformGroup createText(String text, Vector3f position, float size, Color3f color) {
+        
+        // Create 3D font
+        Font font = new Font("Arial", Font.BOLD, 1);
+        Font3D font3D = new Font3D(font, new FontExtrusion());
+        
+        // Create 3D text
+        Text3D text3D = new Text3D(font3D, text);
+        text3D.setAlignment(Text3D.ALIGN_CENTER);
+        
+        // Create shape with the text
+        Shape3D textShape = new Shape3D(text3D, GameObjects.set_Appearance(color));
+        
+        // Create transform group for positioning and scaling
+        TransformGroup textTransformGroup = new TransformGroup();
+        Transform3D textTransform = new Transform3D();
+        
+        // Set position and scale
+        textTransform.setTranslation(position);
+        textTransform.setScale(size);
+        
+        textTransformGroup.setTransform(textTransform);
+        textTransformGroup.addChild(textShape);
+        
+        return textTransformGroup;
     }
 }
